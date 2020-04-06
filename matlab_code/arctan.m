322852ac4a6f401955cad7e41b5d31be2a114a5e
w-guo <weig@uw.edu>
2020-04-05 17:51:44 -0700

    Added nonlinear weighting function

--- matlab_code/arctan.m
+++ matlab_code/arctan.m
@@ -1,4 +1,9 @@
 function [y] = arctan(b_p_data, params)
+% The form of arctan function is taken from the weighting function used in 
+% G. Kusano et al., “Persistence weighted gaussian kernel for topological data 
+% analysis,” in Proc. 33rd Int. Conf. Mach. Learning (ICML’16), 2016. 
+%
+% Author: Wei Guo
 
 x = b_p_data(:,2);
 
